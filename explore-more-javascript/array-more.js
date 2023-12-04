13d38ecfc75ee68dac7a5d0235aebb94397f25c7
Raja Saha <sahatech02@gmail.com>
2023-12-04 13:18:03 +0530

    explore more array

---
+++ explore-more-javascript/array-more.js
@@ -0,0 +1,28 @@
+// 22-1 Module Introduction and isArray, includes, concat
+const country = 'India';
+const age = 52;
+const isIndependent = true;
+const student = {id: 121, class: 11, name:'Agun'};
+const friends = [13, 14, 11, 17, 21, 16, 15, 20];
+function add(num1, num2){
+    return num1, num2;
+}
+console.log(typeof country);
+console.log(typeof age);
+console.log(typeof isIndependent);
+console.log(typeof student);
+// Check array using Array.isArray
+console.log(Array.isArray(friends));
+console.log(typeof add);
+
+// ----------------------------
+console.log(friends.includes(19));
+if(friends.indexOf(252)!==-1){
+
+}
+
+
+// concate
+const newFriendAge = [12,15,13,17]
+const allFriends = newFriendAge.concat(friends);
+console.log(allFriends);
