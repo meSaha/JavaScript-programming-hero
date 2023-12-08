172ca86e0dcbc80bbefeb6bcc9b3315ed4273a49
Raja Saha <sahatech02@gmail.com>
2023-12-08 18:28:12 +0530

    module22 code update

--- explore-more-javascript/discount.js
+++ explore-more-javascript/discount.js
@@ -9,31 +9,59 @@
         101-200   ----> 90 Taka
         200+ ---->70tk
  */
-
-function ticketPrice(ticketQuantity){
-    const frist100Rate = 100;
+    function ticketPrice(ticketQuantity){
+    const first100Rate = 100;
     const second100Rate = 90;
     const restTicketRate = 70;
     if(ticketQuantity <= 100){
-        const price = ticketQuantity * frist100Rate;
+        const price = ticketQuantity * first100Rate;
         return price;
     }
-    else if(ticketQuantity <= 200);{
-    const first100Price = 100 * frist100Rate;
-    const restTicketQuantity = ticketQuantity - 100;
-    const restTicketPrice = restTicketQuantity * second100Rate;
-    const totalPrice = first100Price + restTicketPrice;
-    return totalPrice;
+    else if(ticketQuantity <= 200){
+        const first100Price = 100 * first100Rate;
+        const restTicketQuantity = ticketQuantity - 100;
+        const restTicketPrice = restTicketQuantity * second100Rate;
+        const totalPrice = first100Price + restTicketPrice;
+        return totalPrice;
     }
-    // else {
-    //     const first100Price = 100 * first100Price;
-    //     const second100Rate = 100 * second100Rate;
-    //     const restTicketQuantity = ticketQuantity-200;
-    //     const restTicketPrice = ticketQuantity * restTicketRate;
-    //     const totalCost = first100Price + second100Price + restTicketRate;
-    //     return totalCost;
-    // }
+    else{
+        const first100Price = 100 * first100Rate;
+        const second100Price = 100 * second100Rate;
+        const restTicketQuantity = ticketQuantity - 200;
+        const restTicketPrice = restTicketQuantity * restTicketRate;
+        const totalCost = first100Price + second100Price + restTicketPrice;
+        return totalCost;
+    }
 }
+
 const price = ticketPrice(120);
-console.log('price:',price);
+console.log('price:', price);
+
+
+// function ticketPrice(ticketQuantity){
+//     const frist100Rate = 100;
+//     const second100Rate = 90;
+//     const restTicketRate = 70;
+//     if(ticketQuantity <= 100){
+//         const price = ticketQuantity * frist100Rate;
+//         return price;
+//     }
+//     else if(ticketQuantity <= 200);{
+//     const first100Price = 100 * frist100Rate;
+//     const restTicketQuantity = ticketQuantity - 100;
+//     const restTicketPrice = restTicketQuantity * second100Rate;
+//     const totalPrice = first100Price + restTicketPrice;
+//     return totalPrice;
+//     }
+//     // else {
+//     //     const first100Price = 100 * first100Price;
+//     //     const second100Rate = 100 * second100Rate;
+//     //     const restTicketQuantity = ticketQuantity-200;
+//     //     const restTicketPrice = ticketQuantity * restTicketRate;
+//     //     const totalCost = first100Price + second100Price + restTicketRate;
+//     //     return totalCost;
+//     // }
+// }
+// const price = ticketPrice(120);
+// console.log('price:',price);
     
