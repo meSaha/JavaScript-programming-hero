c2e8288137d69f00b1fae4a263fa38087a6998b4
Raja Saha <sahatech02@gmail.com>
2023-12-15 19:08:12 +0530

    end my 23 module

---
+++ problem-solving.js
@@ -0,0 +1,42 @@
+// 23_5-5 Problem solving priority and get best out of this course
+function compare(a, b) {
+    if (a == b) {
+     return true;
+    } else {
+     return false;
+    }
+   }
+   const result = compare(15, "15");
+   console.log(result);
+
+//    What will be the output of the following JavaScript code? (Hey , Read Carefully)
+
+function compare(a, b) {
+    if (a.toString() === b) {
+     return true;
+    } else {
+     return false;
+    }
+   }
+   const results = compare(25, 25);
+   console.log(results);
+   
+
+   console.log("123" + 123);
+
+
+
+//    What will be the output of the following JavaScript code?
+
+let a ="hi"
+let b="there"
+console.log(a+b)
+
+
+
+// How will you print the numbers from 5 to 100:
+
+/* for(___?___ ; _____?_____ ; ____?____)
+
+let i=5; i<= 100; i++
+*/
